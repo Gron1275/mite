37bb6b0e85e36018741d5ac4f1efc8e01efcffec
Gron1275 <grennongurney@gmail.com>
2019-03-04 20:08:57 -0500

    Add files via upload

---
+++ mite-chisel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Created by: Gron1275
+echo
+echo installing and compiling files
+go get github.com/jpillora/chisel && go build ~/go/src/github.com/jpillora/chisel && go install ~/go/src/github.com/jpillora/chisel
+sudo chmod u+x socat-chisel && sudo cp socat-chisel /usr/local/bin
+sudo chmod u+x socat-tunnel && sudo cp socat-tunnel /usr/local/bin
+sudo chmod u+x chisel-init && sudo cp chisel-init /etc/network/if-up.d
+sudo chmod u+x socat-init && sudo cp socat-init /etc/network/if-up.d
+echo "Make sure to edit the socat-tunnel file and change the server to your own!!"
